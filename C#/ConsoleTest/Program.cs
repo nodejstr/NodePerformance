bc9220c38cf8a714d6f6246c61d121a231c0f885
Salim KAYABAŞI <salim.kayabasi@gmail.com>
2013-02-23 08:35:47 +0200

    sample exe

--- C#/ConsoleTest/Program.cs
+++ C#/ConsoleTest/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication2
+namespace ConsoleTest
 {
     class Program
     {
